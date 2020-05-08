0c5b6a3eef93622b3036687a228bb5559bbdd783
Brokko <brokko@gmail.com>
2020-05-08 20:19:31 +0200

    V1.0.0

---
+++ app/src/main/java/com/github/brokko/youtubedownload/notification/ErrorNotification.java
@@ -0,0 +1,64 @@
+package com.github.brokko.youtubedownload.notification;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.github.brokko.youtubedownload.DownloadActivity;
+import com.github.brokko.youtubedownload.R;
+
+public class ErrorNotification extends NotificationCompat.Builder {
+
+    public static final String ACTION_CLOSE = "ACTION_CLOSE";
+    public static final String ACTION_TOUCH = "ACTION_TOUCH";
+
+    private final IntentFilter intentFilter = new IntentFilter();
+
+    private final NotificationManagerCompat notiManagerCompat;
+
+    private final PendingIntent pendingIntentClose;
+    private final PendingIntent pendingIntentTouch;
+
+    public ErrorNotification(@NonNull Context context) {
+        super(context, DownloadActivity.CHANNEL_ID);
+        notiManagerCompat = NotificationManagerCompat.from(context);
+
+        pendingIntentClose = PendingIntent.getBroadcast(context, 1, new Intent(ACTION_CLOSE), 0);
+        pendingIntentTouch = PendingIntent.getBroadcast(context, 1, new Intent(ACTION_TOUCH), 0);
+
+        intentFilter.addAction(ErrorNotification.ACTION_TOUCH);
+
+        this.setSmallIcon(R.drawable.ic_launcher_foreground);
+        this.setDeleteIntent(pendingIntentClose);
+        this.setAutoCancel(true);
+        this.setSubText("Downloader");
+        this.setContentText("Download fehler! Wiederholen?");
+        this.setContentIntent(pendingIntentTouch);
+        this.setColor(Color.BLACK);
+        this.setPriority(NotificationManager.IMPORTANCE_DEFAULT);
+    }
+
+    public void send() {
+        Notification notification = this.build();
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+
+        notiManagerCompat.notify(1, notification);
+    }
+
+    public void destroy() {
+        notiManagerCompat.cancel(1);
+    }
+
+    @NonNull
+    public IntentFilter getIntentFilter() {
+        return intentFilter;
+    }
+}
---
+++ app/src/main/java/com/github/brokko/youtubedownload/notification/ProgressNotification.java
@@ -0,0 +1,47 @@
+package com.github.brokko.youtubedownload.notification;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.github.brokko.youtubedownload.DownloadActivity;
+import com.github.brokko.youtubedownload.R;
+
+public class ProgressNotification extends NotificationCompat.Builder {
+
+    private final NotificationManagerCompat notiManagerCompat;
+
+    public ProgressNotification(@NonNull Context context) {
+        super(context, DownloadActivity.CHANNEL_ID);
+        notiManagerCompat = NotificationManagerCompat.from(context);
+
+        this.setSmallIcon(R.drawable.ic_launcher_foreground);
+        this.setAutoCancel(true);
+        this.setSubText("Download progress");
+        this.setPriority(NotificationManager.IMPORTANCE_DEFAULT);
+    }
+
+    private void send() {
+        Notification notification = this.build();
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+        notification.flags |= Notification.FLAG_NO_CLEAR;
+
+        notiManagerCompat.notify(0, notification);
+    }
+
+    public void destroy() {
+        notiManagerCompat.cancel(0);
+    }
+
+    public void setProgress(int current, String task, int inQueue, boolean indeterminate) {
+        this.setContentTitle("In queue: "+inQueue);
+        this.setProgress(3, current, indeterminate);
+        this.setContentText(task);
+
+        send();
+    }
+}
